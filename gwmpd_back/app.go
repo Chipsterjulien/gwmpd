ea23953fc46e0eb5e4823ccf59d822d3779c2855
Julien Freyermuth <julien.chipster@gmail.com>
2018-05-03 09:53:20 +0200

    Solve a bug and remove useless comments

--- gwmpd_back/app.go
+++ gwmpd_back/app.go
@@ -139,7 +139,7 @@ func (e *com) getCurrentSong(c *gin.Context) {
 	for {
 		line := <-e.cmdToConsume
 		if bytes.Equal(line, []byte("OK")) {
-			return
+			break
 		}
 
 		first, end := splitLine(&line)
@@ -198,7 +198,7 @@ func (e *com) getPreviousSong(c *gin.Context) {
 	for {
 		line := <-e.cmdToConsume
 		if bytes.Equal(line, []byte("OK")) {
-			return
+			break
 		}
 
 		first, _ := splitLine(&line)
@@ -218,7 +218,7 @@ func (e *com) getNextSong(c *gin.Context) {
 	for {
 		line := <-e.cmdToConsume
 		if bytes.Equal(line, []byte("OK")) {
-			return
+			break
 		}
 
 		first, _ := splitLine(&line)
@@ -238,7 +238,7 @@ func (e *com) getStopSong(c *gin.Context) {
 	for {
 		line := <-e.cmdToConsume
 		if bytes.Equal(line, []byte("OK")) {
-			return
+			break
 		}
 
 		first, _ := splitLine(&line)
@@ -258,7 +258,7 @@ func (e *com) getPlaySong(c *gin.Context) {
 	for {
 		line := <-e.cmdToConsume
 		if bytes.Equal(line, []byte("OK")) {
-			return
+			break
 		}
 
 		first, _ := splitLine(&line)
@@ -278,7 +278,7 @@ func (e *com) getPauseSong(c *gin.Context) {
 	for {
 		line := <-e.cmdToConsume
 		if bytes.Equal(line, []byte("OK")) {
-			return
+			break
 		}
 
 		first, _ := splitLine(&line)
@@ -291,6 +291,24 @@ func (e *com) getPauseSong(c *gin.Context) {
 	c.JSON(200, gin.H{"pauseSong": "ok"})
 }
 
+func (e *com) getPlaylist(c *gin.Context) {
+	log := logging.MustGetLogger("log")
+	e.sendCmdToMPDChan <- []byte("playlistinfo")
+
+	for {
+		line := <-e.cmdToConsume
+		if bytes.Equal(line, []byte("OK")) {
+			break
+		}
+
+		first, _ := splitLine(&line)
+		switch first {
+		default:
+			log.Errorf("In getPlaylist, unknown: \"%s\"\n", first)
+		}
+	}
+}
+
 func (e *com) getStatusMPD(c *gin.Context) {
 	log := logging.MustGetLogger("log")
 	e.sendCmdToMPDChan <- []byte("status")
@@ -298,7 +316,7 @@ func (e *com) getStatusMPD(c *gin.Context) {
 	for {
 		line := <-e.cmdToConsume
 		if bytes.Equal(line, []byte("OK")) {
-			return
+			break
 		}
 
 		first, end := splitLine(&line)
@@ -390,6 +408,7 @@ func (e *com) getStatusMPD(c *gin.Context) {
 			log.Errorf("Unknown: \"%s\"\n", first)
 		}
 	}
+
 	c.JSON(200, gin.H{
 		"consume":        e.info.status.consume,
 		"mixrampdb":      e.info.status.mixrampDB,
@@ -407,11 +426,6 @@ func (e *com) getStatusMPD(c *gin.Context) {
 	})
 }
 
-func (e *com) getPlaylist(c *gin.Context) {
-	// e.sendCmdToMPDChan <- []byte("playlistinfo")
-	// <-e.waitPermissionToSendJSONAtVue
-}
-
 func (e *com) setChangeVolume(c *gin.Context) {
 	log := logging.MustGetLogger("log")
 	var vol volumeForm
@@ -422,7 +436,7 @@ func (e *com) setChangeVolume(c *gin.Context) {
 		for {
 			line := <-e.cmdToConsume
 			if bytes.Equal(line, []byte("OK")) {
-				return
+				break
 			}
 
 			first, _ := splitLine(&line)
@@ -452,7 +466,7 @@ func (e *com) toggleMuteVolume(c *gin.Context) {
 	for {
 		line := <-e.cmdToConsume
 		if bytes.Equal(line, []byte("OK")) {
-			return
+			break
 		}
 
 		first, _ := splitLine(&line)
@@ -494,7 +508,7 @@ func initGin(com *com) {
 		v1.GET("/statusMPD", com.getStatusMPD)
 		v1.GET("/stopSong", com.getStopSong)
 		v1.PUT("/toggleMuteVolume", com.toggleMuteVolume)
-		// v1.GET("/getPlaylist", com.getPlaylist)
+		v1.GET("/getPlaylist", com.getPlaylist)
 	}
 
 	log.Debugf("Port: %d", viper.GetInt("ginserver.port"))
@@ -542,18 +556,6 @@ func startApp() {
 	sendCmdToMPDChan := make(chan []byte, 100)
 	// Commande à traiter en interne
 	cmdToConsume := make(chan []byte, 100)
-	// Autorisation pour envoyer les infos à la vue
-	// waitPermissionToSendJSONAtVue := make(chan bool)
-
-	// currentSong := &mpdCurrentSong{}
-	// status := &mpdStatus{}
-	// stat := &mpdStat{}
-
-	// info := &mpdInfos{
-	// 	currentSong: &mpdCurrentSong{},
-	// 	status:      &mpdStatus{},
-	// 	stat:        &mpdStat{},
-	// }
 
 	com := &com{
 		sendCmdToMPDChan: sendCmdToMPDChan,
@@ -565,7 +567,6 @@ func startApp() {
 			status:      &mpdStatus{},
 			stat:        &mpdStat{},
 		},
-		// waitPermissionToSendJSONAtVue: waitPermissionToSendJSONAtVue,
 	}
 
 	socket := initMPDSocket()
@@ -624,6 +625,7 @@ func eventManagement(com *com) {
 		case <-ticker.C:
 			now := time.Now()
 			delta := now.Sub(past)
+			// If no action during 50s
 			if delta > time.Duration(50)*time.Second {
 				past = now
 				com.sendCmdToMPDChan <- []byte("ping")
@@ -645,199 +647,3 @@ func eventManagement(com *com) {
 		}
 	}
 }
-
-// func event(com *com, waitPermissionToSendJSONAtVue chan<- bool, line *[]byte) {
-// 	log := logging.MustGetLogger("log")
-//
-// 	if log.IsEnabledFor(5) {
-// 		log.Debugf("> %s", string(*line))
-// 	}
-//
-// 	if bytes.Contains(*line, []byte("OK MPD")) {
-// 		return
-// 	} else if bytes.Equal(*line, []byte("OK")) {
-// 		waitPermissionToSendJSONAtVue <- true
-// 		return
-// 	} else if bytes.Contains(*line, []byte("ACK")) {
-// 		waitPermissionToSendJSONAtVue <- true
-// 		return
-// 	}
-//
-// 	// lineSplitted := strings.Split(string(*line), ":")
-// 	// end := strings.TrimLeft(strings.Join(lineSplitted[1:], ":"), " ")
-// 	// switch lineSplitted[0] {
-// 	// case "Album":
-// 	// 	com.info.album = end
-// 	// case "albums":
-// 	// 	i, err := strconv.Atoi(end)
-// 	// 	if err != nil {
-// 	// 		log.Warningf("Unable to convert \"albums\" %s", end)
-// 	// 		return
-// 	// 	}
-// 	// 	com.info.albums = i
-// 	// case "Artist":
-// 	// 	com.info.artist = end
-// 	// case "artists":
-// 	// 	i, err := strconv.Atoi(end)
-// 	// 	if err != nil {
-// 	// 		log.Warningf("Unable to convert \"artists\" %s", end)
-// 	// 		return
-// 	// 	}
-// 	// 	com.info.artists = i
-// 	// case "audio":
-// 	// 	com.info.audio = end
-// 	// case "bitrate":
-// 	// 	com.info.bitrate = end
-// 	// case "consume":
-// 	// 	if end == "1" {
-// 	// 		com.info.consume = true
-// 	// 	} else {
-// 	// 		com.info.consume = false
-// 	// 	}
-// 	// case "Date":
-// 	// 	com.info.date = end
-// 	// case "db_playtime":
-// 	// 	com.info.dbPlaytime = end
-// 	// case "db_update":
-// 	// 	com.info.dbUpdate = end
-// 	// case "duration":
-// 	// 	f, err := strconv.ParseFloat(end, 64)
-// 	// 	if err != nil {
-// 	// 		log.Warningf("Unable to convert \"duration\" %s", end)
-// 	// 		return
-// 	// 	}
-// 	// 	com.info.duration = f
-// 	// case "elapsed":
-// 	// 	f, err := strconv.ParseFloat(end, 64)
-// 	// 	if err != nil {
-// 	// 		log.Warningf("Unable to convert \"elapsed\" %s", end)
-// 	// 		return
-// 	// 	}
-// 	// 	com.info.elapsed = f
-// 	// case "file":
-// 	// 	com.info.file = end
-// 	// case "Genre":
-// 	// 	com.info.genre = end
-// 	// case "Id":
-// 	// 	i, err := strconv.Atoi(end)
-// 	// 	if err != nil {
-// 	// 		log.Warningf("Unable to convert \"Id\" %s", end)
-// 	// 		return
-// 	// 	}
-// 	// 	com.info.id = i
-// 	// case "Last-Modified":
-// 	// 	com.info.lastModified = end
-// 	// case "mixrampdb":
-// 	// 	f, err := strconv.ParseFloat(end, 64)
-// 	// 	if err != nil {
-// 	// 		log.Warningf("Unable to convert \"mixrampdb\" %s", end)
-// 	// 		return
-// 	// 	}
-// 	// 	com.info.mixrampDB = f
-// 	// case "Name":
-// 	// 	com.info.name = end
-// 	// case "nextsong":
-// 	// 	i, err := strconv.Atoi(end)
-// 	// 	if err != nil {
-// 	// 		log.Warningf("Unable to convert \"nextsong\" %s", end)
-// 	// 		return
-// 	// 	}
-// 	// 	com.info.nextSong = i
-// 	// case "nextsongid":
-// 	// 	i, err := strconv.Atoi(end)
-// 	// 	if err != nil {
-// 	// 		log.Warningf("Unable to convert \"nextsongid\" %s", end)
-// 	// 		return
-// 	// 	}
-// 	// 	com.info.nextSongID = i
-// 	// case "playlist":
-// 	// 	i, err := strconv.Atoi(end)
-// 	// 	if err != nil {
-// 	// 		log.Warningf("Unable to convert \"playlist\" %s", end)
-// 	// 		return
-// 	// 	}
-// 	// 	com.info.playlist = i
-// 	// case "playlistlength":
-// 	// 	i, err := strconv.Atoi(end)
-// 	// 	if err != nil {
-// 	// 		log.Warningf("Unable to convert \"playlistlength\" %s", end)
-// 	// 		return
-// 	// 	}
-// 	// 	com.info.playlistLength = i
-// 	// case "playtime":
-// 	// 	com.info.playtime = end
-// 	// case "Pos":
-// 	// 	i, err := strconv.Atoi(end)
-// 	// 	if err != nil {
-// 	// 		log.Warningf("Unable to convert \"Pos\" %s", end)
-// 	// 		return
-// 	// 	}
-// 	// 	com.info.pos = i
-// 	// case "random":
-// 	// 	if end == "1" {
-// 	// 		com.info.random = true
-// 	// 	} else {
-// 	// 		com.info.random = false
-// 	// 	}
-// 	// case "repeat":
-// 	// 	if end == "1" {
-// 	// 		com.info.repeat = true
-// 	// 	} else {
-// 	// 		com.info.repeat = false
-// 	// 	}
-// 	// case "single":
-// 	// 	if end == "1" {
-// 	// 		com.info.single = true
-// 	// 	} else {
-// 	// 		com.info.single = false
-// 	// 	}
-// 	// case "song":
-// 	// 	i, err := strconv.Atoi(end)
-// 	// 	if err != nil {
-// 	// 		log.Warningf("Unable to convert \"song\" %s", end)
-// 	// 		return
-// 	// 	}
-// 	// 	com.info.song = i
-// 	// case "songs":
-// 	// 	i, err := strconv.Atoi(end)
-// 	// 	if err != nil {
-// 	// 		log.Warningf("Unable to convert \"songs\" %s", end)
-// 	// 		return
-// 	// 	}
-// 	// 	com.info.songs = i
-// 	// case "songid":
-// 	// 	i, err := strconv.Atoi(end)
-// 	// 	if err != nil {
-// 	// 		log.Debug("coin")
-// 	// 		log.Warningf("Unable to convert \"songid\" %s", end)
-// 	// 		return
-// 	// 	}
-// 	// 	com.info.songID = i
-// 	// case "state":
-// 	// 	com.info.state = end
-// 	// case "Time":
-// 	// 	com.info.timeSong = end
-// 	// case "time":
-// 	// 	com.info.timeElapsed = end
-// 	// case "Title":
-// 	// 	com.info.title = end
-// 	// case "Track":
-// 	// 	i, err := strconv.Atoi(end)
-// 	// 	if err != nil {
-// 	// 		log.Warningf("Unable to convert \"Track\" %s", end)
-// 	// 		return
-// 	// 	}
-// 	// 	com.info.track = i
-// 	// case "uptime":
-// 	// 	com.info.uptime = end
-// 	// case "volume":
-// 	// 	i, err := strconv.Atoi(end)
-// 	// 	if err != nil {
-// 	// 		log.Warningf("Unable to convert \"volume\" %s", end)
-// 	// 		return
-// 	// 	}
-// 	// 	com.info.volume = i
-// 	// default:
-// 	// 	log.Errorf("Unknown: \"%s\"\n", lineSplitted[0])
-// 	// }
-// }
