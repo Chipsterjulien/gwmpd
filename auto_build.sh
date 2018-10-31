227eb04a81bf6503f1d2a5651c162f768e99097c
Chipster <julien.chipster@gmail.com>
2018-10-31 18:24:54 +0100

    Put some comments when you build app

--- auto_build.sh
+++ auto_build.sh
@@ -3,12 +3,15 @@
 mkdir -p back front
 
 # build backend
+echo "Install/Update go packages"
 go get -u github.com/gin-gonic/gin
 go get -u github.com/itsjamie/gin-cors
 go get -u github.com/op/go-logging
 go get -u github.com/spf13/viper
 go get -u github.com/appleboy/gin-jwt
 
+# build backend
+echo "Build backend"
 cd gwmpd_back/
 go build -o gwmpdBack
 mv gwmpdBack ../back
@@ -16,6 +19,7 @@ mv cfg ../back
 cd ..
 
 # build frontend
+echo "Build frontend"
 cd gwmpd_front/Gwmpd/
 yarn
 yarn build
@@ -24,3 +28,5 @@ mv * ../../../front
 cd ../../..
 rm -rf gwmpd_back gwmpd_front
 rm License README.md auto_build.sh
+
+echo "End !"
